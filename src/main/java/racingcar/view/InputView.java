423b4b043dedd11beeeaf491f9fd0a555cc35d3e
Huiyeongkim <huiyeong9619@naver.com>
2025-10-26 06:50:30 +0900

    feat: add InputView for car names and round count

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,27 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class InputView {
+
+    private static final String CAR_NAMES_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ROUND_COUNT_INPUT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private static final String DELIMITER = ",";
+
+    public List<String> inputCarNames() {
+        System.out.println(CAR_NAMES_INPUT_MESSAGE);
+        String input = Console.readLine();
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .toList();
+    }
+
+    public int inputRoundCount() {
+        System.out.println(ROUND_COUNT_INPUT_MESSAGE);
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+}
