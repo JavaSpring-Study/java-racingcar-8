0b6e1b43bfb2d9b27c4a356b0a19452a1a410efe
HeeJeongSeo <heejung9965@naver.com>
2025-10-26 17:21:39 +0900

    feat : domain 전체 핵심기능로직 구현

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,5 +1,34 @@
 package racingcar.domain;
 
+import java.util.List;
+
+import racingcar.views.OutputView;
+
+//전체 게임의 진행 제어 로직 구현
 public class RacingGame {
 
+		private Cars cars;
+		private int attemptCount;
+		
+		
+		//이름 시도횟수 초기화
+		public void initialize(List<String> carNames, int attemptCount) {
+			this.cars = new Cars(carNames);
+			this.attemptCount = attemptCount;
+		}
+		
+		public void start(OutputView outputView) {
+			outputView.printStartMessage();
+			
+			for (int i = 0; i < attemptCount; i++) {
+				cars.moveAll(); 
+				printRoundResult(outputView);
+			}
+		}
+		
+		private void printRoundResult(OutputView outputView) {
+			for(Car car : cars.getCars()) {
+				outputView.printRoundResult(car.getName(), car.getPosition());
+			}
+		}
 }
--- src/main/java/racingcar/views/OutputView.java
+++ src/main/java/racingcar/views/OutputView.java
@@ -17,6 +17,7 @@ public class OutputView {
 	}
 	
 	public void printWinners(List<String> winners) {
+		System.out.println();
 		System.out.println("최종 우승자 : " + String.join(", ", winners));
 	}
 }
