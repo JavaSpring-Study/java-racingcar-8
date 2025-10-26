2c5d3333746a5c52b0204a1e0401435f67b3bafa
HeeJeongSeo <heejung9965@naver.com>
2025-10-26 16:25:01 +0900

    feat: inputView 계층 구현 및 예외처리

--- src/main/java/racingcar/views/InputView.java
+++ src/main/java/racingcar/views/InputView.java
@@ -1,5 +1,15 @@
 package racingcar.views;
 
-public class InputView {
+import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.utils.Validator;
+
+public class InputView {
+	//사용자에서 자동차 이름과 시도 횟수를 입력받는 역할 
+	public List<String> readCarNames() {
+		System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
+		String input = Console.readLine();
+		return Validator.validateAndParseNames(input);
+		}
 }
