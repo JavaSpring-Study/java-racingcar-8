3037a2b78babea1cfe5a129222f73f5304e3e949
Lee Ye Jin <jene0028@gmail.com>
2025-10-27 16:59:57 +0900

    feat(controller): GameController 및 Application 구성, 게임 실행 흐름 완성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,20 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.model.Validator;
+import racingcar.model.generator.RandomValueGenerator;
+import racingcar.model.generator.ValueGenerator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Validator validator = new Validator();
+        ValueGenerator generator = new RandomValueGenerator();
+
+        GameController controller = new GameController(inputView, outputView, validator, generator);
+        controller.run();
     }
 }
---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,45 @@
+package racingcar.controller;
+
+import racingcar.model.car.Cars;
+import racingcar.model.game.RacingGame;
+import racingcar.model.generator.ValueGenerator;
+import racingcar.model.Validator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class GameController {
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final Validator validator;
+    private final ValueGenerator generator;
+
+    public GameController(
+            InputView inputView,
+            OutputView outputView,
+            Validator validator,
+            ValueGenerator generator
+    ) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.validator = validator;
+        this.generator = generator;
+    }
+
+    public void run() {
+        List<String> names = inputView.readCarNames();
+        validator.validateNames(names);
+
+        String attemptsInput = inputView.readAttemptCount();
+        int attempts = validator.validateAttempts(attemptsInput);
+
+        Cars cars = Cars.valueOf(names);
+        RacingGame game = RacingGame.of(cars, attempts);
+
+        outputView.printResultHeader();
+        game.play(generator);
+        outputView.printRoundResult(game.getRoundResults());
+        outputView.printWinners(game.returnWinners());
+    }
+}
