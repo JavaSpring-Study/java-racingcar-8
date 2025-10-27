89932a463cdb14d063fdff5598408082dab71dc5
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-10-27 23:23:36 +0900

    feat: Application과 CarController 실행 흐름 구현

--- src/main/java/racingcar/controller/CarController.java
+++ src/main/java/racingcar/controller/CarController.java
@@ -1,14 +1,35 @@
-package racingcar.domain;
+package racingcar.controller;
+
+import racingcar.domain.*;
+import racingcar.external.RandomNumberGenerator;
+import racingcar.view.Input;
+import racingcar.view.Output;
 
 import java.util.List;
-
-import racingcar.view.Input;
 
 public class CarController {
 
 	public void run() {
 		Input input = new Input();
-		List<String> carname = input.readCarNameInput();
-		Integer count = input.readCount();
+		Output output = new Output();
+
+		// 자동차 이름 입력
+		List<String> carNames = input.readCarNameInput();
+
+		// 생성자 참조 대신 람다로 안전하게 변환
+		List<Car> cars = carNames.stream()
+			.map(name -> new Car(name))
+			.toList();
+
+		// 시도 횟수 입력
+		int tryCount = input.readCount();
+
+		// 게임 실행
+		RacingController game = new RacingController(cars, new RandomNumberGenerator());
+		game.play(tryCount, output);
+
+		// 최종 우승자 출력
+		List<String> winners = game.getWinners();
+		output.printWinners(winners);
 	}
 }
