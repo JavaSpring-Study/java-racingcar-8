1aed89cc55eba8d7d608003c26e6cba7f67d43f6
Lee Ye Jin <jene0028@gmail.com>
2025-10-27 16:59:26 +0900

    test(validator): Validator 인스턴스 기반 구조에 맞게 단위 테스트 수정

--- src/test/java/racingcar/model/ValidatorTest.java
+++ src/test/java/racingcar/model/ValidatorTest.java
@@ -10,62 +10,65 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class ValidatorTest {
 
+    private final Validator validator = new Validator();
+
     @Test
     void 이름이_null이거나_빈문자열이면_예외() {
-        assertThatThrownBy(() -> Validator.validateNames(null))
+        assertThatThrownBy(() -> validator.validateNames(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessages.INPUT_EMPTY);
 
-        assertThatThrownBy(() -> Validator.validateNames(List.of()))
+        assertThatThrownBy(() -> validator.validateNames(List.of()))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessages.INPUT_EMPTY);
     }
 
     @Test
     void 이름이_공백이거나_5자초과이면_예외() {
-        assertThatThrownBy(() -> Validator.validateNames(List.of(" ")))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> validator.validateNames(List.of(" ")))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ExceptionMessages.INPUT_EMPTY);
 
-        assertThatThrownBy(() -> Validator.validateNames(List.of("abcdef")))
+        assertThatThrownBy(() -> validator.validateNames(List.of("abcdef")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessages.INVALID_NAME_LENGTH);
     }
 
     @Test
     void 이름이_중복이면_예외() {
-        assertThatThrownBy(() -> Validator.validateNames(List.of("pobi", "pobi")))
+        assertThatThrownBy(() -> validator.validateNames(List.of("pobi", "pobi")))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessages.DUPLICATE_NAME);
     }
 
     @Test
     void 시도횟수가_null이거나_공백이면_예외() {
-        assertThatThrownBy(() -> Validator.validateAttempts(null))
+        assertThatThrownBy(() -> validator.validateAttempts(null))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessages.INPUT_EMPTY);
 
-        assertThatThrownBy(() -> Validator.validateAttempts(" "))
+        assertThatThrownBy(() -> validator.validateAttempts(" "))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessages.INPUT_EMPTY);
     }
 
     @Test
     void 시도횟수가_숫자가_아니면_예외() {
-        assertThatThrownBy(() -> Validator.validateAttempts("abc"))
+        assertThatThrownBy(() -> validator.validateAttempts("abc"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessages.INVALID_NUMBER_FORMAT);
     }
 
     @Test
     void 시도횟수가_0이하면_예외() {
-        assertThatThrownBy(() -> Validator.validateAttempts("0"))
+        assertThatThrownBy(() -> validator.validateAttempts("0"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessages.INVALID_NUMBER_RANGE);
     }
 
     @Test
     void 시도횟수가_유효하면_정수로_반환한다() {
-        int count = Validator.validateAttempts("3");
+        int count = validator.validateAttempts("3");
         assertThat(count).isEqualTo(3);
     }
 }
